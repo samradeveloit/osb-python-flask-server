32943e972c9eae3afea18ea11cde1da0c8e07291
Samra Riaz <samrariaz2003@gmail.com>
2026-05-05 18:11:55 +0500

    Refactor authorization controller to implement bearer CRN authentication and update Swagger API security definitions

--- swagger_server/controllers/authorization_controller.py
+++ swagger_server/controllers/authorization_controller.py
@@ -1,22 +1,50 @@
-from typing import List
-"""
-controller generated to handled auth operation described at:
-https://connexion.readthedocs.io/en/latest/security.html
-"""
-def check_basicAuth(username, password, required_scopes):
-    return {'test_key': 'test_value'}
+import os
 
-def check_bearer_header(apikey, required_scopes):
-    if not apikey:
+import jwt
+
+
+IBM_IAM_ISSUER = "https://iam.cloud.ibm.com/identity"
+IBM_IAM_JWKS_URL = os.environ.get("IBM_IAM_JWKS_URL", "https://iam.cloud.ibm.com/identity/keys")
+BROKER_CRN = os.environ.get("BROKER_CRN", "")
+JWK_CLIENT = jwt.PyJWKClient(IBM_IAM_JWKS_URL)
+
+
+def _normalize_crn(value):
+    if not value:
+        return ""
+    if value.startswith("crn-crn:"):
+        return value[len("crn-"):]
+    return value
+
+
+def check_bearer_crn(apikey, required_scopes):
+    if not apikey or not BROKER_CRN:
         return None
 
     parts = apikey.split(" ", 1)
     token = parts[1] if len(parts) == 2 and parts[0].lower() == "bearer" else apikey
-    if token:
-        return {'uid': 'user'}
-    return None
+    if not token:
+        return None
 
-def check_bearerAuth(token):
-    if token:
-        return {'uid': 'user'}
-    return None
+    signing_key = JWK_CLIENT.get_signing_key_from_jwt(token)
+    claims = jwt.decode(
+        token,
+        signing_key.key,
+        algorithms=["RS256"],
+        issuer=IBM_IAM_ISSUER,
+        options={"verify_aud": False},
+    )
+
+    expected_crn = _normalize_crn(BROKER_CRN)
+    token_identities = {
+        _normalize_crn(claims.get("id")),
+        _normalize_crn(claims.get("iam_id")),
+    }
+
+    if expected_crn not in token_identities:
+        return None
+
+    return {
+        "uid": expected_crn,
+        "token": claims,
+    }
