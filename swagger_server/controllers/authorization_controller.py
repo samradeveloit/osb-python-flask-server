a7899e4e170cd114d742c1fc5bc8a90174c8779c
Samra Riaz <samrariaz2003@gmail.com>
2026-05-05 16:26:05 +0500

    Refactor bearer authentication function and update Swagger API definition

--- swagger_server/controllers/authorization_controller.py
+++ swagger_server/controllers/authorization_controller.py
@@ -6,6 +6,16 @@ https://connexion.readthedocs.io/en/latest/security.html
 def check_basicAuth(username, password, required_scopes):
     return {'test_key': 'test_value'}
 
+def check_bearer_header(apikey, required_scopes):
+    if not apikey:
+        return None
+
+    parts = apikey.split(" ", 1)
+    token = parts[1] if len(parts) == 2 and parts[0].lower() == "bearer" else apikey
+    if token:
+        return {'uid': 'user'}
+    return None
+
 def check_bearerAuth(token):
     if token:
         return {'uid': 'user'}
